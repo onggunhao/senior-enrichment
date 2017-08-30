4352e9d46515c529da48fb76a770997a648b5f6c
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-08-31 02:25:22 +0700

    Added models for students and campuses

---
+++ db/models/campus.js
@@ -0,0 +1,13 @@
+'use strict';
+var Sequelize = require('sequelize')
+var db = require('../index.js')
+
+module.exports = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING  // In case of large iamges
+  }
+})
